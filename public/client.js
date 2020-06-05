7546864b654a8ab49479dca6199e7fd7039317b8
ZeevBenni <benni.lamm@gmail.com>
2020-06-05 22:10:16 +0200

    v1 create User & get user

--- public/client.js
+++ public/client.js
@@ -1,16 +1,17 @@
 $(function () {
   loadSurveys();
+  searchuser('e0862925-405a-4dce-b2a8-a3515df1a88f');
 
-  $("form").submit(function (event) {
+  $('form').submit(function (event) {
     event.preventDefault();
-    var name = $("input#name").val();
+    var name = $('input#name').val();
     var uuid = create_UUID();
-    var modern_klassisch = $("input#modern_klassisch").val();
-    var unterhaltsam_serioes = $("input#unterhaltsam_serioes").val();
-    var answer = $("input#answer").val();
-    var color = $("input#color").val();
+    var modern_klassisch = $('input#modern_klassisch').val();
+    var unterhaltsam_serioes = $('input#unterhaltsam_serioes').val();
+    var answer = $('input#answer').val();
+    var color = $('input#color').val();
     $.post(
-      "/surveys?" +
+      '/surveys?' +
         $.param({
           name: name,
           uuid: uuid,
@@ -36,36 +37,49 @@ $(function () {
   });
 
   function loadSurveys() {
-    $.get("/surveys", function (surveys) {
-      $("ul#surveys li").remove();
+    $.get('/surveys', function (surveys) {
+      $('ul#surveys li').remove();
       surveys.forEach(function (survey) {
-        $("<li></li>")
+        $('<li></li>')
           .text(
             survey.name +
-              " " +
+              ' ' +
               survey.uuid +
-              " " +
+              ' ' +
               survey.modern_klassisch +
-              " " +
+              ' ' +
               survey.unterhaltsam_serioes +
-              " " +
+              ' ' +
               survey.answer +
-              " " +
+              ' ' +
               survey.color
           )
-          .appendTo("ul#surveys");
+          .appendTo('ul#surveys');
       });
     });
   }
+
+  function searchuser(uuid) {
+    $.get(
+      '/searchuser?' +
+        $.param({
+          uuid: uuid,
+        }),
+      function (user) {
+        alert(user.name);
+      }
+    ).fail(function () {
+      alert('nicht gefunden');
+    });
+  }
 });
+
 function create_UUID() {
   var dt = new Date().getTime();
-  var uuid = "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function (
-    c
-  ) {
+  var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
     var r = (dt + Math.random() * 16) % 16 | 0;
     dt = Math.floor(dt / 16);
-    return (c == "x" ? r : (r & 0x3) | 0x8).toString(16);
+    return (c == 'x' ? r : (r & 0x3) | 0x8).toString(16);
   });
   return uuid;
 }
@@ -74,23 +88,23 @@ function create_UUID() {
 
 function gofromtoslide(actSlide, nextslide) {
   var x = document.getElementById(nextslide);
-  if (x.style.display == "none") {
-    x.style.display = "block";
+  if (x.style.display == 'none') {
+    x.style.display = 'block';
   } else {
-    x.style.display = "none";
+    x.style.display = 'none';
   }
 
   var y = document.getElementById(actSlide);
-  if (y.style.display == "block") {
-    y.style.display = "none";
+  if (y.style.display == 'block') {
+    y.style.display = 'none';
   } else {
-    y.style.display = "block";
+    y.style.display = 'block';
   }
 }
 
 //slider
-var slider = document.getElementById("modern_klassisch");
-var output = document.getElementById("demo");
+var slider = document.getElementById('modern_klassisch');
+var output = document.getElementById('demo');
 
 output.innerHTML = slider.value;
 
--- server.js
+++ server.js
@@ -1,34 +1,72 @@
-const express = require("express");
-const bodyParser = require("body-parser");
+const express = require('express');
+const bodyParser = require('body-parser');
 
 // setup webserver
 const app = express();
 const server = app.listen(process.env.PORT || 3000, () => {
   console.log(`Listening on port: ${server.address().port}`);
 });
-app.use(express.static("public"));
+app.use(express.static('public'));
 app.use(bodyParser.json());
 
 // setup database
 // Security note: the database is saved to the file `db.json` on the local filesystem.
 // It's deliberately placed in the `.data` directory which doesn't get copied if someone remixes the project.
-const low = require("lowdb");
-const FileSync = require("lowdb/adapters/FileSync");
-const adapter = new FileSync(".data/db.json");
+const low = require('lowdb');
+const FileSync = require('lowdb/adapters/FileSync');
+const adapter = new FileSync('.data/db.json');
 const db = low(adapter);
 
 // set default data
-db.defaults({ surveys: [] }).write();
+db.defaults({ surveys: [], users: [] }).write();
+
+/**
+ * Add User
+ * @query username
+ */
+app.post('/newuser', function (request, response) {
+  const username = request.query.username;
+  const uuid = create_UUID();
+  db.get('users').push({ name: username, uuid: uuid, favColor: '', doneColors: [] }).write();
+  response.sendStatus(200);
+});
+
+/**
+ * Check for existing User
+ * @query uuid
+ */
+app.get('/searchuser', function (request, response) {
+  const uuid = request.query.uuid;
+  const user = db.get('users').find({ uuid: uuid }).value();
+  if (user) {
+    response.status(200).send(user);
+  } else {
+    response.sendStatus(404);
+  }
+});
+
+/**
+ * Random Farbe generieren
+ * @param uuid
+ *
+ * 1. random farbe aus datenset
+ * 2. prüfen ob Spieler bereits farbe gespielt
+ * (3.) wenn bereits gespielt bei 1. neu beginnen
+ */
+
+app.get('/randomcolor/:uuid', function (request, response) {
+  const uuid = request.params.uuid;
+});
 
 // get all surveys
-app.get("/surveys", function (request, response) {
-  response.json(db.get("surveys"));
+app.get('/surveys', function (request, response) {
+  response.json(db.get('surveys'));
 });
 
 // creates a new entry in the surveys collection with the submitted values
-app.post("/surveys", function (request, response) {
+app.post('/surveys', function (request, response) {
   console.log(request.query);
-  db.get("surveys")
+  db.get('surveys')
     .push({
       name: request.query.name,
       uuid: request.query.uuid,
@@ -38,14 +76,31 @@ app.post("/surveys", function (request, response) {
       color: request.query.color,
     })
     .write();
-  console.log("New survey inserted");
+  console.log('New survey inserted');
   response.sendStatus(200);
 });
 
-app.get("/colordata");
+app.get('/colordata');
 
 // clear surveys
-app.get("/clear", function (request, response) {
-  db.get("surveys").remove().write();
-  response.send("Database cleared");
+app.get('/clear', function (request, response) {
+  db.get('surveys').remove().write();
+  response.send('Database cleared');
 });
+
+// FUNCTIONS
+
+/**
+ *Generate Random uuid
+ *
+ * @returns uuid
+ */
+function create_UUID() {
+  var dt = new Date().getTime();
+  var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+    var r = (dt + Math.random() * 16) % 16 | 0;
+    dt = Math.floor(dt / 16);
+    return (c == 'x' ? r : (r & 0x3) | 0x8).toString(16);
+  });
+  return uuid;
+}
